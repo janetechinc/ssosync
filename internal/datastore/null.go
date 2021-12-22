eafd68e87ca959ede180057812c4b519e9bd4b1a
Christopher Liebman <liebman@zod.com>
2021-12-22 12:41:08 -0800

    fix client_test (add null datastore)

---
+++ internal/datastore/null.go
@@ -0,0 +1,23 @@
+package datastore
+
+import (
+)
+
+type nullDatastore struct {
+	*baseDatastore
+}
+
+func NewNullDatastore() Datastore {
+	return &nullDatastore{
+		baseDatastore: newBaseDatastore(),
+	}
+}
+
+func (ds *nullDatastore) Load() (error) {
+	return nil
+}
+
+func (ds *nullDatastore) Store() error {
+	return nil
+}
+
